abf6e8e2bd5ab43274477f881922d8ae6700ecbb
Byron Escobar <byes0811@gmail.com>
2022-12-12 03:40:27 -0500

    Se adicionan y mejora endpoints para actualizar y eliminar odontologo

--- src/main/java/com/proyectofinal/clinicaodontologica/controllers/OdontologoController.java
+++ src/main/java/com/proyectofinal/clinicaodontologica/controllers/OdontologoController.java
@@ -1,5 +1,6 @@
 package com.proyectofinal.clinicaodontologica.controllers;
 
+import com.proyectofinal.clinicaodontologica.exceptions.ResourceNotFoundException;
 import com.proyectofinal.clinicaodontologica.models.Odontologo;
 import com.proyectofinal.clinicaodontologica.services.OdontologoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,8 +33,13 @@ public class OdontologoController {
         return odontologoService.buscarPorId(id);
     }
 
-    @DeleteMapping("/delete")
-    public void deletePorRequestParam(@RequestParam Integer id){
+    @DeleteMapping("/eliminar")
+    public void eliminarOdontologoRP(@RequestParam Integer id){
+        odontologoService.eliminar(id);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminarOdontologoPath(@PathVariable("id") Integer id){
         odontologoService.eliminar(id);
     }
 
@@ -41,5 +47,12 @@ public class OdontologoController {
     @ResponseBody
     public List<Odontologo> buscarTodos(){
         return odontologoService.buscarTodos();
+    }
+
+    @PutMapping("/actualizar")
+    @ResponseBody
+    public Odontologo actualizarOdontologo(@RequestBody Odontologo odontologo) throws ResourceNotFoundException {
+
+        return odontologoService.actualizarOdontologo(odontologo);
     }
 }
--- src/main/java/com/proyectofinal/clinicaodontologica/models/Odontologo.java
+++ src/main/java/com/proyectofinal/clinicaodontologica/models/Odontologo.java
@@ -1,13 +1,10 @@
 package com.proyectofinal.clinicaodontologica.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 @AllArgsConstructor
